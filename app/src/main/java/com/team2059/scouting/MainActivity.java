c16c55064d0963b9bbdd9d5fa86eabcd59b8aeb0
aamijar <aamijar230@gmail.com>
2019-09-14 00:06:19 -0400

    init commit

---
+++ app/src/main/java/com/team2059/scouting/MainActivity.java
@@ -0,0 +1,16 @@
+package com.team2059.scouting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
