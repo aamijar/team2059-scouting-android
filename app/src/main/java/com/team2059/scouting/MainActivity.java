59a1c6e9a1c73ae2bdf3ba036e0effa9f1674294
aamijar <aamijar230@gmail.com>
2019-10-28 16:32:11 -0400

    new widgets tested

--- app/src/main/java/com/team2059/scouting/MainActivity.java
+++ app/src/main/java/com/team2059/scouting/MainActivity.java
@@ -3,6 +3,10 @@ package com.team2059.scouting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        final EditText comments = (EditText)findViewById(R.id.entry_comment);
+        final TextView test1 = (TextView)findViewById(R.id.textView);
+        final
+
+        Button button = (Button)findViewById(R.id.submit);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v)
+            {
+                String user_entry = comments.getText().toString();
+                test1.setText(user_entry);
+
+            }
+        });
+
         
     }
 }
