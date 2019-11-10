58cc6edf99d593093dbf3625e6cc9ea758f74caf
aamijar <aamijar230@gmail.com>
2019-11-09 22:57:40 -0500

    new save Ameen branch

--- app/src/main/java/com/team2059/scouting/MasterView.java
+++ app/src/main/java/com/team2059/scouting/MasterView.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TableLayout;
+import android.content.Context;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MasterView extends AppCompatActivity {
 
-    //widgets
-    private ImageButton imageButton = (ImageButton) findViewById(R.id.refresh);
-    private TableLayout tableLayout = (TableLayout) findViewById(R.id.table_master);
+    //declare widgets
+
+
+    //primitive types
 
     //default setup
     @Override
@@ -19,13 +26,45 @@ public class MasterView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master_view);
 
+        ImageButton imageButton = findViewById(R.id.refresh);
+        final Context context = getApplicationContext();
 
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TableLayout tableLayout = findViewById(R.id.table_master);
+                TableRow row = new TableRow(context);
+                TextView text = new TextView(context);
+
+                text.setText("team2059");
+                row.addView(text);
+                tableLayout.addView(row);
+                //refreshData();
+            }
+        });
 
 
     }
 
     public void refreshData()
     {
+        Context context = getApplicationContext();
+        TableLayout tableLayout = findViewById(R.id.table_master);
+        ArrayList<String> readData = new ArrayList<String>();
+        readData = FileManager.readFromFile(context);
+
+        TableRow row = new TableRow(context);
+        TextView text = new TextView(context);
+
+        for(String s: readData)
+        {
+            text.setText(s);
+            row.addView(text);
+            tableLayout.addView(row);
+        }
+
 
 
     }
